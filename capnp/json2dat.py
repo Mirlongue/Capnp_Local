f06ef6ac578d27bad070454a67915fe3d3842223
Xiaohui Dong <tianfuyuming@outlook.com>
2025-11-26 23:14:44 +0800

    2025 1126 capnp modify

--- capnp/json2dat.py
+++ capnp/json2dat.py
@@ -32,23 +32,21 @@ def json_to_txt(data, indent_level = 0):
 
 if __name__ == '__main__':
 
-    script_path = os.path.abspath(__file__)
-    current_dir = os.path.dirname(script_path)
+    current_dir = os.getcwd()
 
     json_path = os.path.join(current_dir, 'sample.json')
     with open(json_path, 'r', encoding='utf-8') as file:
         json_data = json.load(file)
+
     print(json_data)
 
     txt_data = json_to_txt(json_data)
 
-    txt_path = os.path.join(current_dir, 'sample.txt')
-    with open(txt_path, 'w', encoding='utf-8') as file:
+    with open('sample.txt', 'w', encoding='utf-8') as file:
         file.write(txt_data)
 
-    dat_path = os.path.join(current_dir, 'sample.dat')
-    with open(txt_path, 'r') as input_file:
-        with open(dat_path, 'w') as output_file:
+    with open("sample.txt", 'r') as input_file:
+        with open("sample.dat", 'w') as output_file:
             subprocess.run(cmd, stdin = input_file, stdout = output_file, stderr = subprocess.PIPE, shell = True)
 
-    os.remove(txt_path)
+    os.remove("sample.txt")
--- main.cpp
+++ main.cpp
@@ -12,10 +12,10 @@
 #include<kj/string-tree.h>
 #include"capnp/sample.capnp.h"
 
-const std::string PATH_ROOT = "D:\\Reposity_github\\Capnp_Local\\build\\"; // <project_root>/build
+const std::string PATH_ROOT = "D:/Reposity_github/Capnp_Local";
 
-void dump_capnp_for_bit(std::shared_ptr<capnp::MallocMessageBuilder> tmp_mbuilder_ptr, std::string name) {
-    std::string path = PATH_ROOT + name;
+void capnp_dump(std::shared_ptr<capnp::MallocMessageBuilder> tmp_mbuilder_ptr, std::string name) {
+    std::string path = PATH_ROOT + "/build/" + name;
     std::ofstream ofs(path, std::ios::binary);
     if (ofs) {
         kj::Array<capnp::word> words = capnp::messageToFlatArray(*tmp_mbuilder_ptr);
@@ -29,8 +29,8 @@ void dump_capnp_for_bit(std::shared_ptr<capnp::MallocMessageBuilder> tmp_mbuilde
 }
 
 template<typename T>
-std::shared_ptr<capnp::MallocMessageBuilder> get_capnp_for_bit(std::string const name) {
-    std::string path = PATH_ROOT + name;
+std::shared_ptr<capnp::MallocMessageBuilder> capnp_load_from_dump(std::string const name) {
+    std::string path = PATH_ROOT +  "/build/" + name;
     std::ifstream ifs = std::ifstream{path};
     std::shared_ptr<capnp::MallocMessageBuilder> tmp_mbuilder_ptr =
         std::make_shared<capnp::MallocMessageBuilder>();
@@ -46,54 +46,74 @@ std::shared_ptr<capnp::MallocMessageBuilder> get_capnp_for_bit(std::string const
     return tmp_mbuilder_ptr;
 }
 
+template<typename T>
+std::shared_ptr<capnp::MallocMessageBuilder> capnp_load_from_text(std::string const name) {
+    std::string path = PATH_ROOT + "/capnp/" + name;
+    std::ifstream ifs = std::ifstream{path};
+    std::shared_ptr<capnp::MallocMessageBuilder> tmp_mbuilder_ptr =
+        std::make_shared<capnp::MallocMessageBuilder>();
+    if (ifs) {
+        std::vector<char> buffer(std::istreambuf_iterator<char>{ifs}, std::istreambuf_iterator<char>{});
+        capnp::word * words = reinterpret_cast<capnp::word *>(buffer.data());
+        int word_count = buffer.size() / sizeof(capnp::word);
+        const kj::ArrayPtr<const capnp::word> segment(words, word_count);
+        kj::ArrayPtr<const kj::ArrayPtr<const capnp::word>> segments(&segment, 1);
+        capnp::SegmentArrayMessageReader reader(segments);
+        auto tmp_reader = reader.getRoot<T>();
+        tmp_mbuilder_ptr->setRoot(tmp_reader);
+        ifs.close();
+    }
+    return tmp_mbuilder_ptr;
+}
 
 int main() {
-    std::shared_ptr<capnp::MallocMessageBuilder> test0_mbuilder_ptr =
-        std::make_shared<capnp::MallocMessageBuilder>();
-    sample::Sample::Builder test0_builder =
-        test0_mbuilder_ptr->initRoot<sample::Sample>();
+
+    std::shared_ptr<capnp::MallocMessageBuilder> test0_mbuilder_ptr = std::make_shared<capnp::MallocMessageBuilder>();
+    sample::Sample::Builder test0_builder = test0_mbuilder_ptr->initRoot<sample::Sample>();
+
     test0_builder.setIntegerSample(999);
     test0_builder.setStSample("hellow world!");    // std::string  or  char st[<length>]    char * st (no safe)
+    int arr[4] = {0, 1, 2, 3};
+    test0_builder.setArrSample(arr);
     capnp::List<int, capnp::Kind::PRIMITIVE>::Builder arr_builder = test0_builder.initArrSample(4);
-    for(int i = 0; i < 4; i++) {
-        arr_builder.set(i, i);    // test0_builder.getArrSample().set(i,i);
-    }
-    // int arr[4] = {0, 1, 2, 3};
-    // test0_builder.setArrSample(arr);
-    sample::Sample::Reader test0_reader = test0_builder.asReader();
-    // dump_capnp_for_bit(test0_mbuilder_ptr, "test0");
+    for(int i = 0; i < 4; i++) { arr_builder.set(i, i); }    // test0_builder.getArrSample().set(i,i);
 
-    std::shared_ptr<capnp::MallocMessageBuilder> test1_mbuilder_ptr =
-        std::make_shared<capnp::MallocMessageBuilder>();
-    test1_mbuilder_ptr->setRoot(test0_reader);
-    // std::shared_ptr<capnp::MallocMessageBuilder> test1_mbuilder_ptr =
-    //     get_capnp_for_bit<sample::Sample>("test0");
-    sample::Sample::Builder test1_builder =
-        test1_mbuilder_ptr->getRoot<sample::Sample>();
+    capnp_dump(test0_mbuilder_ptr, "test0.dat");
+
+    std::shared_ptr<capnp::MallocMessageBuilder> test1_mbuilder_ptr = std::make_shared<capnp::MallocMessageBuilder>();
+    test1_mbuilder_ptr->setRoot(test0_builder.asReader());
+    sample::Sample::Builder test1_builder = test1_mbuilder_ptr->getRoot<sample::Sample>();
     sample::Sample::Reader test1_reader = test1_builder.asReader();
 
     int integer_sample = test1_reader.getIntegerSample();
     std::string st_sample(test1_reader.getStSample().begin(), test1_reader.getStSample().end());
     int arr_sample[4];
-    for(int i = 0; i < 4; i++) {
-        arr_sample[i] = test1_reader.getArrSample()[i];
-    }
+    for(int i = 0; i < 4; i++) { arr_sample[i] = test1_reader.getArrSample()[i]; }
     int enum_sample = sample::Sample::ENUM_SAMPLE;
-    std::cout << "##########" << std::endl;
+
+    std::cout << "#### print test1 ####" << std::endl;
     std::cout << "integer_sample: " << integer_sample << std::endl;
     std::cout << "st_sample: " << st_sample << std::endl;
     std::cout << "arr_sample:";
-    for(int i = 0; i < 4; i++) {
-        std::cout << " " << arr_sample[i];
-    }
+    for(int i = 0; i < 4; i++) { std::cout << " " << arr_sample[i]; }
     std::cout << std::endl;
-    std::cout << "enum_sample: " << enum_sample << std::endl;
+    std::cout << "enum_sample: " << enum_sample << std::endl << std::endl;
 
-    std::string sample_flat(test1_reader.toString().flatten().cStr());
-    int sample_size(test1_reader.toString().flatten().size());
-    std::cout << "##########" << std::endl;
-    std::cout << "sample_flat: " << sample_flat << std::endl;
-    std::cout << "sample_size: " << sample_size << std::endl;
+    std::shared_ptr<capnp::MallocMessageBuilder> test2_mbuilder_ptr = capnp_load_from_dump<sample::Sample>("test0.dat");
+    sample::Sample::Builder test2_builder = test2_mbuilder_ptr->getRoot<sample::Sample>();
+    std::string test2_flat(test2_builder.asReader().toString().flatten().cStr());
+    int test2_size(test2_builder.asReader().toString().flatten().size());
+    std::cout << "#### capnp load from dump ####" << std::endl;
+    std::cout << "test2_flat: " << test2_flat << std::endl;
+    std::cout << "test2_size: " << test2_size << std::endl << std::endl;
+
+    std::shared_ptr<capnp::MallocMessageBuilder> test3_mbuilder_ptr = capnp_load_from_text<sample::Sample>("sample.dat");
+    sample::Sample::Builder test3_builder = test3_mbuilder_ptr->getRoot<sample::Sample>();
+    std::string test3_flat(test3_builder.asReader().toString().flatten().cStr());
+    int test3_size(test3_builder.asReader().toString().flatten().size());
+    std::cout << "#### capnp load from text ####" << std::endl;
+    std::cout << "test3_flat: " << test3_flat << std::endl;
+    std::cout << "test3_size: " << test3_size << std::endl;
 
     return 0;
 }
